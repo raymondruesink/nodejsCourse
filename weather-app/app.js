660b9d4c32607ae07a6dea1fab5a2a29d912f269
Raymond Ruesink <raymond.ruesink@adp.com>
2019-12-10 11:00:36 +0100

    6.32

--- weather-app/app.js
+++ weather-app/app.js
@@ -2,10 +2,12 @@ const request = require('request')
 
 const secretKey = '59468cbcb4d51b662ca662fa8498da33'
 
-const url = 'https://api.darksky.net/forecast/59468cbcb4d51b662ca662fa8498da33/37.8267,-122.4233'
+const url = 'https://api.darksky.net/forecast/59468cbcb4d51b662ca662fa8498da33/51.9226052,5.9333145?units=si&lang=nl'
 
-request({ url: url }, (error, response) => {
-    const data = JSON.parse(response.body)
-    console.log(data.currently)
+request({
+    url: url,
+    json: true
+}, (error, response) => {
+    console.log(response.body.daily.data[0].summary + ' It is currently ' + response.body.currently.temperature + ' degrees out. There is a ' + response.body.currently.precipProbability + '% chance of rain.')
 })
 
