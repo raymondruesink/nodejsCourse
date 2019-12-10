c8727af43aa9f0b62d16fe06fbd82d7fe53da493
Raymond Ruesink <raymond.ruesink@adp.com>
2019-12-10 18:10:14 +0100

    6.35

---
+++ playground/4-callbacks.js
@@ -0,0 +1,36 @@
+// setTimeout(() => {
+//     console.log('Two seconds are up')
+// }, 2000)
+
+// const names = ['Andrew', 'Jen', 'Jess']
+// const shortNames = names.filter((name) => {
+//     return name.length <= 4
+// })
+
+// const geoCode = (address, callback) => {
+//     setTimeout (() => {
+//         const data = {
+//             latitude: 0,
+//             longitude: 0
+//         }
+
+//         callback(data)
+
+//     }, 2000)
+
+    
+// }
+
+// geoCode('Philadelphia', (data) => {
+//     console.log(data)
+// })
+
+const add = (a, b, callback) => {
+    setTimeout(() => {
+       callback(a + b)     
+    }, 2000)
+}
+
+add(1, 4, (sum => {
+    console.log(sum)
+}))
