2341078202a5f5b81ca42ac05a62671402868de0
Raymond Ruesink <raymond.ruesink@adp.com>
2019-12-10 11:51:53 +0100

    6.33

--- weather-app/app.js
+++ weather-app/app.js
@@ -3,11 +3,25 @@ const request = require('request')
 const secretKey = '59468cbcb4d51b662ca662fa8498da33'
 
 const url = 'https://api.darksky.net/forecast/59468cbcb4d51b662ca662fa8498da33/51.9226052,5.9333145?units=si&lang=nl'
+const mapBoxToken = 'pk.eyJ1IjoicmF5bW9uZDE0NzcyIiwiYSI6ImNrM3pwOHM2NjFzazEzbW8xbHBzMXIwNnAifQ.7AfTN3tlkAQl8dQWvd6vtA'
+const mapBoxPlace = 'huissen huttenstraat 32'
+const mapBoxUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + mapBoxPlace + '.json?access_token=' + mapBoxToken + '&limit=1'
+
+
+// request({
+//     url: url,
+//     json: true
+// }, (error, response) => {
+//     console.log(response.body.daily.data[0].summary + ' It is currently ' + response.body.currently.temperature + ' degrees out. There is a ' + response.body.currently.precipProbability + '% chance of rain.')
+// })
 
 request({
-    url: url,
+    url: mapBoxUrl,
     json: true
 }, (error, response) => {
-    console.log(response.body.daily.data[0].summary + ' It is currently ' + response.body.currently.temperature + ' degrees out. There is a ' + response.body.currently.precipProbability + '% chance of rain.')
+    const f = response.body.features[0]
+    console.log('Place : ' + f.place_name)
+    console.log('Longitude : ' + f.center[0])
+    console.log('Latitude : ' + f.center[1])
+    
 })
-
